166da6a97a2452625dc9e13d523e614d5966fc5b
unknown <ki_haissam@esi.dz>
2025-06-05 03:01:01 +0200

    Implement logout logic

--- src/app/ui/admin/home/nav-links.tsx
+++ src/app/ui/admin/home/nav-links.tsx
@@ -1,6 +1,7 @@
-'use client'
-import Link from "next/link"
-import { usePathname } from 'next/navigation';
+'use client';
+
+import Link from "next/link";
+import { usePathname, useRouter } from 'next/navigation';
 import clsx from 'clsx';
 
 export type LinkItem = {
@@ -8,39 +9,45 @@ export type LinkItem = {
   href: string;
 };
 
-
 export default function NavLinks({
-    links
+  links
 }: {
-    links: LinkItem[];
-}
-){
-    const pathname = usePathname();
+  links: LinkItem[];
+}) {
+  const pathname = usePathname();
+  const router = useRouter();
 
-    return(
-        <ul className="flex md:flex-row gap-4 md:gap-8 text-sm items-center flex-col">
+  const handleLogout = () => {
+    localStorage.removeItem('token');     
+    router.push('/login');                
+  };
+
+  return (
+    <ul className="flex md:flex-row gap-4 md:gap-8 text-sm items-center flex-col">
+      {links.map((link) => (
+        <li
+          key={link.name}
+          className={clsx(
+            "border-b-2 border-transparent hover:border-primary duration-75",
             {
-                links.map((link) => {
-                    return(
-                        <li key={link.name}
-                                className={ clsx(
-                                    "border-b-2 border-transparent hover:border-primary duration-75",
-                                    {
-                                        "": link.href === pathname
-                                    }
-                                )
-                                }
-                        >
-                            <Link 
-                                href ={link.href}
-                            >
-                                <p>{link.name}</p>
-                            </Link>
-                        </li>
-                    )
-                })
+              "": link.href === pathname,
             }
-            <Link href='/login' className="cursor-pointer border-2 hover:bg-white hover:border-primary hover:text-primary px-6 py-2 rounded-md font-semibold border-white bg-gray-400 text-white">Logout</Link>
-        </ul>
-    )
+          )}
+        >
+          <Link href={link.href}>
+            <p>{link.name}</p>
+          </Link>
+        </li>
+      ))}
+
+      <li>
+        <button
+          onClick={handleLogout}
+          className="cursor-pointer border-2 hover:bg-white hover:border-primary hover:text-primary px-6 py-2 rounded-md font-semibold border-white bg-gray-400 text-white"
+        >
+          Logout
+        </button>
+      </li>
+    </ul>
+  );
 }
